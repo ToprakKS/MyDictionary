c7de4237e6b51e655fd5203c8544acd12aaebf31
MONSTER <MONSTER@DESKTOP-0HS3G36>
2021-01-22 18:53:24 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<string> sozluk = new MyDictionary<string>();
+            sozluk.Add("Kitap");
+            sozluk.Add("Masa");
+            sozluk.Add("Laptop");
+            sozluk.Add("Defter");
+            sozluk.Add("Mouse");
+            Console.WriteLine(sozluk.Count);
+            
+
+
+        }
+    }
+
+}
